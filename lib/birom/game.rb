91670fb2f15b7f2954726ac110b8efde1f36cf90
Sebastian de Castelberg <sebu@kpricorn.org>
2013-03-05 15:27:17 +0100

    implement #take

--- lib/birom/game.rb
+++ lib/birom/game.rb
@@ -1,4 +1,5 @@
 require 'birom/birom'
+require 'birom/turn'
 require 'birom/grid'
 require 'birom/grid_utils'
 require 'birom/game_player'
@@ -13,6 +14,7 @@ module Birom
     attr_reader :moves
     attr_reader :players
     attr_reader :turn
+    attr_reader :winners
 
     def initialize
       @red = Birom::RED
@@ -25,6 +27,7 @@ module Birom
       @numberOfPlayers = 2
       @turn = TurnUndefined
       @moves = [@red]
+      @winners = []
     end
 
     def join(user)
@@ -55,6 +58,73 @@ module Birom
         p == player
       end
     end
+
+    def take(turn)
+      unless @running
+        raise Exception.new("Game is not running")
+      end
+
+      if @turn != turn.gamePlayer
+        raise Exception.new("Not player's turn")
+      end
+
+      counter = turn.counter
+      points = []
+      gridCopy = GridUtils.copyGrid(@grid)
+
+      gridCopy.validate!(counter)
+      counter.triangles.each do |triangle|
+        gridCopy.set(triangle)
+      end
+      gridCopy.fillBorderTriangles
+
+      gridCopy.getPointTriangles(counter).each do |pointTriangle|
+        gridCopy.set pointTriangle
+        points << pointTriangle
+      end
+
+      turn.gamePlayer.points += points.length
+      captured = gridCopy.getCapturedTriangles(turn.counter.triangles)
+
+      pcMap = {}
+      # TODO: duplicated triangles are returned
+      captured = gridCopy.getCapturedTriangles(turn.counter.triangles)
+      captured.each do |t|
+        unless t.player.nil?
+          pcMap[t.player] ||= []
+          pcMap[t.player] << "#{t.u}/#{t.v}/#{t.w}"
+        end
+        t.type = Triangle::TRI_TYPE_COMMON
+        t.player = nil
+        gridCopy.set(t)
+      end
+
+      # increment opponent's counter (captured triangles)
+      pcMap.each do |player, coords|
+        opponents(player).each do |opponent|
+            opponent.countersLeft += coords.uniq.length / 4
+        end
+      end
+
+      # decrement current move
+      turn.gamePlayer.countersLeft -= 1
+
+      @turn = Game.getNextTurn(@turn, @players)
+
+      if @turn.nil?
+        @running = false
+      end
+
+      @winners.push(*@players.select do |player|
+        gridCopy.isEncircledBy(@red.triangles.first, player)
+      end)
+
+      @running = @winners.empty?
+
+      @grid = GridUtils.copyGrid(gridCopy)
+      turn
+    end
+
     def self.getNextTurn(currentPlayer, players)
       pidx = players.index(currentPlayer)
       players.rotate(pidx + 1).select do |player|
